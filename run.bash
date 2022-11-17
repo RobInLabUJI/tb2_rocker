825e6774ee2cd52b33d3b8afeb3fa9f195a226b1
ecervera <Enric Cervera>
2022-11-17 09:34:08 +0100

    add model folder

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rocker --nvidia --name tb2_rocks --volume config:/config --network tb2_rocker_default --x11 tb2:kinetic bash
+rocker --nvidia --name tb2_rocks --volume config:/config --volume gazebo_models_worlds_collection:/gazebo_models_worlds_collection --volume Dataset-of-Gazebo-Worlds-Models-and-Maps:/Dataset-of-Gazebo-Worlds-Models-and-Maps --network tb2_rocker_default --x11 tb2:kinetic bash
 
